3dbad0e9576b7281138bed984e97689548887a68
Lucas Gretti <lucassgretti@outlook.com>
2023-10-17 11:43:04 -0300

    Refatoracao e inclusao de campos na classe arquivo

--- Models/Arquivo.cs
+++ Models/Arquivo.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using NotaSys.Models.Enums;
 
 namespace NotaSys.Models
 {
     public class Arquivo
     {
         public XDocument ArquivoXml { get; set; }
-        public string Id { get; set; }
 
-        public Arquivo(XDocument arquivoXml, String id)
-        {
+        public string Id { get; private set; }
+        public UfEnum UnidadeFederativa { get; private set; }
+        public string Cnpj { get; private set; }
+
+        public Arquivo(XDocument arquivoXml) {
             ArquivoXml = arquivoXml;
-            Id = id;
+
+            GravaCampos();
+        }
+
+        public void GravaCampos () {
+            //Recupera a tag infNFe
+            XNamespace ns = "http://www.portalfiscal.inf.br/nfe";
+            XElement infNFe = ArquivoXml.Element(ns + "nfeProc").Element(ns + "NFe").Element(ns + "infNFe");
+
+            //Pela tag busca o Atributo Id e grava em uma variável caso haja o valor
+            if (infNFe != null) {
+                XAttribute idAttr = infNFe.Attribute("Id");
+
+                if (idAttr != null) {
+                    Id = idAttr.Value;
+                }
+            }
+
+             UfEnum.TryParse(Id.Substring(3, 2), out UfEnum UnidadeFederativa);
+
+            Cnpj = Id.Substring(9, 14);
         }
     }
 }
---
+++ Models/Enums/UnidadeFederativaEnum.cs
@@ -0,0 +1,31 @@
+﻿namespace NotaSys.Models.Enums {
+    public enum UfEnum {
+        Acre = 12,
+        Alagoas = 27,
+        Amapa = 16,
+        Amazonas = 13,
+        Bahia = 29,
+        Ceara = 23,
+        DistritoFederal = 53,
+        EspiritoSanto = 32,
+        Goias = 52,
+        Maranhao = 21,
+        MatoGrosso = 51,
+        MatoGrossoDoSul = 50,
+        MinasGerais = 31,
+        Para = 15,
+        Paraiba = 25,
+        Parana = 41,
+        Pernambuco = 26,
+        Piauí = 22,
+        RioGrandedoNorte = 24,
+        RioGrandedoSul = 43,
+        RiodeJaneiro = 33,
+        Rondonia = 11,
+        Roraima = 14,
+        SantaCatarina = 42,
+        SaoPaulo = 35,
+        Sergipe = 28,
+        Tocantins = 17
+    }
+}
